1eb0d2d9c2f2a3b4698eddfd5e4087577ee897bc
Jean Marcos Francisco <jfrancisco@alunos.utfpr.edu.br>
2021-02-07 14:38:05 -0300

    themes dark e light

--- minha-carteira/src/App.tsx
+++ minha-carteira/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import GlobalStyles from './Styles/GlobalStyles';
+import {ThemeProvider} from 'styled-components';
 
 import Layout  from './components/Layout'
+import dark from './Styles/themes/dark';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme = {dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
 
--- minha-carteira/src/components/Aside/styles.ts
+++ minha-carteira/src/components/Aside/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div `
     grid-area: AS;
-    background-color: #252A48;
+    background-color: ${props => props.theme.colors.secondary};
 
 
 `;
